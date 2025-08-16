70995983c963c33f0ca25bb88a4b9413f2b47142
Shaurya Srivastava <103360556+Thunderer9506@users.noreply.github.com>
2025-08-16 18:24:34 +0530

    Day 18 Completed

---
+++ Day 18/33_Search_Roteted_Sorted_Array.py
@@ -0,0 +1,24 @@
+def search(nums, target):
+    st = 0
+    end = len(nums)-1
+    while (st <= end):
+        mid = st+(end-st)//2
+
+        if (nums[mid] == target):
+            return mid
+        
+        if (nums[st] <= nums[mid]):
+            if(nums[st] <= target and target <= nums[mid]):
+                end = mid-1
+            else:
+                st = mid +1
+            
+        
+        else:
+            if(nums[mid] <= target and target <= nums[end]):
+                st = mid+1
+            else:
+                end = mid -1
+    return -1
+
+print(search([4,5,6,7,0,1,2],0))
